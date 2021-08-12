a49b1e419bc8ded4b27ddfa8aa5d3385b769f723
Anastasia Semenko <nasssemenko@gmail.com>
2021-08-12 19:23:41 +0300

    Final version

--- src/com/epam/training/strings/service/FileIO.java
+++ src/com/epam/training/strings/service/FileIO.java
@@ -1,7 +1,5 @@
 package com.epam.training.strings.service;
 
-import com.epam.training.strings.domain.Text;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -9,7 +7,6 @@ import java.nio.file.Paths;
 public class FileIO {
     public static String fileRead(String path) {
         try {
-//            String contents = new String(Files.readAllBytes(Paths.get(path)));
             return new String(Files.readAllBytes(Paths.get(path)));
         } catch (IOException e) {
             System.err.println("Во время загрузки текста произошла ошибка!");
